f12b3903ff8f9c9e415c843e7543cab5ca38f0c5
Emmanuel  M.  Ndaliro <33391934+kram254@users.noreply.github.com>
2023-09-19 20:42:19 +0300

    Changes

--- insta_scroll.py
+++ insta_scroll.py
@@ -1,49 +1,67 @@
 import pyautogui as pg
-import keyboard as key
-import time 
+import time
 import random
+import keyboard as key
 
 time.sleep(10)
-# print(pg.position())
 
-pg.leftClick(421, 758, 1, 3)
+# Click to focus on the Instagram search bar
+pg.click(421, 758, 1, 3)
 time.sleep(1)
 pg.typewrite("www.instagram.com")
 time.sleep(1)
 pg.press("enter")
 
-pg.leftClick(71, 124, 1, 5)
+# Wait for the page to load
+time.sleep(5)
 
-pg.doubleClick(578, 767)
-time.sleep(2)
+# Click to focus on the Instagram window
+pg.click(71, 124, 1, 5)
 
-pg.leftClick(934, 538, 1, 5)
-# pg.press('down') 
-# time.sleep(4)  
+num_iterations = 50
 
-for _ in range(25):
-        pg.press('down') 
-        time.sleep(1)
+for _ in range(num_iterations):
+    pg.doubleClick(603, 414)
+    time.sleep(2)
+
+    # Efficiently scroll down within 3 seconds
+    scroll_start_time = time.time()
+    while time.time() - scroll_start_time < 3:
+        pg.hotkey('down')  # Use the down arrow key to scroll
+        time.sleep(0.1)  # Adjust sleep time as needed
+
+    pg.doubleClick(603, 414)
+    time.sleep(2)
+
+
+
+
+# import pyautogui as pg
+# import keyboard as key
+# import time 
+# import random
+
+# time.sleep(10)
+# # print(pg.position())
+
+# pg.leftClick(421, 758, 1, 3)
+# time.sleep(1)
+# pg.typewrite("www.instagram.com")
+# time.sleep(1)
+# pg.press("enter")
+
+# pg.leftClick(71, 124, 1, 5)
+
+# num_iterations = 10
+
+# for _ in range(num_iterations):
+#     pg.doubleClick(603, 414)
+#     time.sleep(2)
+
+#     for _ in range(25):
+#         pg.press('down') 
+#         time.sleep(5)
         
-pg.doubleClick(578, 767)
-time.sleep(2)
+#     pg.doubleClick(603, 414)
+#     time.sleep(2)
 
-
-# for i in range(2500):
-#     # Like button position
-#     pg.doubleClick(399, 395)
-#     time.sleep(3)
-
-#     # # position of the comment box
-#     # pg.leftClick(1011, 688, 1)
-#     # message = random.choice(list)
-#     # pg.typewrite(message)
-#     # time.sleep(1)      
-#     # pg.press("enter")
-#     # time.sleep(5)
-      
-#     # locating NEXT button
-#     pg.leftClick(1320, 396)
-#     time.sleep(3)
-
-# print("I'm Done biiih!!")
